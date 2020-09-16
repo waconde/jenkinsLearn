f01efbb1d9615e124a78f688176633c5028c76bc
side.wang <side.wang@percent.cn>
2020-09-16 15:03:34 +0800

    本地初始化 spring boot 项目

---
+++ src/main/java/side/waconde/jenkinsLearn/JenkinsLearnApplication.java
@@ -0,0 +1,13 @@
+package side.waconde.jenkinsLearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsLearnApplication.class, args);
+	}
+
+}
